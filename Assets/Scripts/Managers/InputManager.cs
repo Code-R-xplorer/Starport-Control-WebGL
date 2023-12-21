479b90c47ff1e59acdaa018d053a114d2911706a
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-12-21 13:00:49 +0000

    Setup Player Controls

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using Controls;
+using UnityEngine;
+
+namespace Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        public static InputManager Instance { get; private set; }
+        private PlayerControls _playerControls;
+        
+        public delegate void BaseAction();
+        public delegate void BoolBaseAction(bool canceled);
+
+        public event BoolBaseAction OnPrimary;
+
+        private void Awake()
+        {
+            _playerControls = new PlayerControls();
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            _playerControls.Controls.Primary.started += context => { OnPrimary?.Invoke(context.canceled); };
+            _playerControls.Controls.Primary.canceled += context => { OnPrimary?.Invoke(context.canceled); };
+        }
+
+        private void OnEnable()
+        {
+            _playerControls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerControls.Disable();
+        }
+    }
+}
